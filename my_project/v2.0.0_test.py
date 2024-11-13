34d411d34ba655c1b544f78703f6dffd256716ca
svutborg <svu@ucn.dk>
2024-11-13 08:45:19 +0100

    Added version 2

---
+++ my_project/v2.0.0_test.py
@@ -0,0 +1,12 @@
+from time import sleep
+import pixel
+
+print("Test file version 2.0")
+sleep(0.5)
+pixel.color((0, 50, 0))
+sleep(0.5)
+pixel.color((0, 0, 0))
+sleep(0.5)
+pixel.color((0, 50, 0))
+sleep(0.5)
+pixel.color((0, 0, 0))
